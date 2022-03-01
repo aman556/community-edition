78b36314c358d7c7abafeafe6246d4361b8e8f8f
Aman Sharma <amansh@vmware.com>
2022-03-01 12:05:46 +0530

    testing
    Signed-off-by: Aman Sharma <amansh@vmware.com>

--- hack/choco/test/choco-upgrade-test.ps1
+++ hack/choco/test/choco-upgrade-test.ps1
@@ -13,13 +13,30 @@ if ((Test-Path env:GITHUB_TOKEN) -eq $False) {
   throw "GITHUB_TOKEN environment variable is not set"
 }
 
-$parentDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
 $TCE_REPO = "https://github.com/vmware-tanzu/community-edition" 
 $TCE_REPO_RELEASES_URL = "https://github.com/vmware-tanzu/community-edition/releases"
 $TCE_WINDOWS_ZIP_FILE="tce-windows-amd64-${version}.zip"
 $TCE_CHECKSUMS_FILE = "tce-checksums.txt"
 
 Write-Host "${parentDir}" -ForegroundColor Cyan
+
+# Use --depth 1 once https://github.com/cli/cli/issues/2979#issuecomment-780490392 get resolve
+git clone "${TCE_REPO}"
+
+cd community-edition
+ 
+PR_BRANCH="update-tce-to-${version}-${RANDOM}"
+ 
+# Random number in branch name in case there's already some branch for the version update,
+# though there shouldn't be one. There could be one if the other branch's PR tests failed and didn't merge
+git checkout -b "${PR_BRANCH}"
+
+# setup
+git config user.name aman556
+git config user.email amansharma14041998@gmail.com
+
+$parentDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 # Testing for current release
 & "${parentDir}\e2e-test.ps1"
@@ -58,7 +75,19 @@ Set-Content -Path .\tools\chocolateyinstall.ps1 -Value $textchocoinstall
 & "${parentDir}\e2e-test.ps1"
 
 # Updating files for current version
-Set-Content -Path .\tanzu-community-edition.nuspec -NoNewline -Value $temptextnuspec
-Set-Content -Path .\tools\chocolateyinstall.ps1 -NoNewline -Value $temptextchocoinstall
+#Set-Content -Path .\tanzu-community-edition.nuspec -NoNewline -Value $temptextnuspec
+#Set-Content -Path .\tools\chocolateyinstall.ps1 -NoNewline -Value $temptextchocoinstall
 
 Remove-Item "${parentDir}/tce-checksums.txt"
+
+git add hack/choco/tools/chocolateyinstall.ps1
+git add hack/choco/tanzu-community-edition.nuspec
+ 
+git commit -s -m "auto-generated - update tce choco install scripts for version ${version}"
+ 
+git push origin "${PR_BRANCH}"
+ 
+gh pr create --repo ${TCE_REPO} --title "auto-generated - update tce choco install scripts for version ${version}" --body "auto-generated - update tce choco install scripts for version ${version}"
+ 
+gh pr merge --repo ${TCE_REPO} "${PR_BRANCH}" --squash --delete-branch --auto
+ 
