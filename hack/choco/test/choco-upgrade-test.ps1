170cd284b520ffa69a9d893741b35b3eeffd586d
Aman Sharma <amansh@vmware.com>
2022-03-01 18:37:48 +0530

    testing
    Signed-off-by: Aman Sharma <amansh@vmware.com>

--- hack/choco/test/choco-upgrade-test.ps1
+++ hack/choco/test/choco-upgrade-test.ps1
@@ -109,7 +109,7 @@ git add tanzu-community-edition.nuspec
  
 git commit -s -m "auto-generated - update tce choco install scripts for version ${version}"
  
-git push origin "${PR_BRANCH}"
+git push -u origin "${PR_BRANCH}"
  
 gh pr create --repo ${TCE_REPO} --title "auto-generated - update tce choco install scripts for version ${version}" --body "auto-generated - update tce choco install scripts for version ${version}"
  
