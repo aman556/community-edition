dc34cb8bdbb7e93ef0aa2fd4a88b415e5f272d41
Aman Sharma <amansh@vmware.com>
2022-03-02 11:44:17 +0530

    testing
    Signed-off-by: Aman Sharma <amansh@vmware.com>

--- hack/choco/test/choco-upgrade-test.ps1
+++ hack/choco/test/choco-upgrade-test.ps1
@@ -10,17 +10,17 @@ param (
 $ErrorActionPreference = 'Stop';
 
 if ((Test-Path env:GITHUB_TOKEN) -eq $False) {
-  throw 'GITHUB_TOKEN environment variable is not set'
+  throw "GITHUB_TOKEN environment variable is not set"
 }
 
 $temp_dir = Join-Path $Env:Temp $(New-Guid); New-Item -Type Directory -Path $temp_dir | Out-Null
  
 Push-Location $temp_dir
 
-$TCE_REPO = 'https://github.com/aman556/community-edition'
-$TCE_REPO_RELEASES_URL = 'https://github.com/vmware-tanzu/community-edition/releases'
-$TCE_WINDOWS_ZIP_FILE='tce-windows-amd64-${version}.zip'
-$TCE_CHECKSUMS_FILE = 'tce-checksums.txt'
+$TCE_REPO = "https://github.com/aman556/community-edition"
+$TCE_REPO_RELEASES_URL = "https://github.com/vmware-tanzu/community-edition/releases"
+$TCE_WINDOWS_ZIP_FILE="tce-windows-amd64-${version}.zip"
+$TCE_CHECKSUMS_FILE = "tce-checksums.txt"
 
 
 # Use --depth 1 once https://github.com/cli/cli/issues/2979#issuecomment-780490392 get resolve
@@ -28,31 +28,31 @@ git clone $TCE_REPO
 
 cd community-edition/hack/choco
 $RANDOM = Get-Random
-$PR_BRANCH = 'update-tce-to-$version-' + $RANDOM
+$PR_BRANCH = "update-tce-to-$version-$RANDOM"
  
 git checkout -b $PR_BRANCH
 git config -global credential.helper unset
 
 
 # setup
-git config --global --replace-all user.name 'aman556'
-git config --global --replace-all user.email 'amansharma14041998@gmail.com'
+git config --global --replace-all user.name "aman556"
+git config --global --replace-all user.email "amansharma14041998@gmail.com"
 
 
 # Testing for current release
 & test\e2e-test.ps1
 
-Write-Host 'Checking if the necessary files exist for the TCE $version release'
+Write-Host "Checking if the necessary files exist for the TCE $version release"
 
-invoke-webrequest '${TCE_REPO_RELEASES_URL}/download/${version}/${TCE_WINDOWS_ZIP_FILE}' -DisableKeepAlive -UseBasicParsing -Method head
-invoke-webrequest '${TCE_REPO_RELEASES_URL}/download/${version}/${TCE_CHECKSUMS_FILE}' -OutFile test/tce-checksums.txt
+invoke-webrequest "${TCE_REPO_RELEASES_URL}/download/${version}/${TCE_WINDOWS_ZIP_FILE}" -DisableKeepAlive -UseBasicParsing -Method head
+invoke-webrequest "${TCE_REPO_RELEASES_URL}/download/${version}/${TCE_CHECKSUMS_FILE}" -OutFile test/tce-checksums.txt
 
-$Checksum64 = ((Select-String -Path './test/tce-checksums.txt' -Pattern 'tce-windows-amd64-${version}.zip').Line.Split(''))[0]
+$Checksum64 = ((Select-String -Path ""./test/tce-checksums.txt" -Pattern "tce-windows-amd64-${version}.zip").Line.Split(" ""))[0]
 
 # Updating the version in tanzu-community-edition-temp.nuspec file
 $textnuspec = Get-Content .\tanzu-community-edition.nuspec -Raw
 $temptextnuspec = Get-Content .\tanzu-community-edition.nuspec -Raw 
-$Regex = [Regex]::new('(?<=<version>)(.*)(?=<\/version>)')
+$Regex = [Regex]::new("(?<=<version>)(.*)(?=<\/version>)")
 $oldVersion = $Regex.Match($textnuspec)
 $textnuspec = $textnuspec.Replace( $oldVersion.value  , $version.Substring(1) )
 Set-Content -Path .\tanzu-community-edition.nuspec -Value $textnuspec
@@ -61,12 +61,12 @@ Set-Content -Path .\tanzu-community-edition.nuspec -Value $textnuspec
 # Updating the version in chocolateyinstall.ps1 file
 $textchocoinstall = Get-Content .\tools\chocolateyinstall.ps1 -Raw 
 $temptextchocoinstall = Get-Content .\tools\chocolateyinstall.ps1 -Raw 
-$Regex = [Regex]::new('(?<=releaseVersion = ')(.*)(?=')')
+$Regex = [Regex]::new("(?<=releaseVersion = ')(.*)(?=')")
 $oldVersion = $Regex.Match($textchocoinstall)
 $textchocoinstall = $textchocoinstall.Replace( $oldVersion.value  , $version )
 
 # Updating the Checksum64 in chocolateyinstall.ps1 file
-$Regex = [Regex]::new('(?<=checksum64 = ')(.*)(?=')')
+$Regex = [Regex]::new("(?<=checksum64 = ')(.*)(?=')")
 $oldChecksum64 = $Regex.Match($textchocoinstall)
 $textchocoinstall = $textchocoinstall.Replace( $oldChecksum64.value  , $Checksum64 )
 
@@ -80,11 +80,11 @@ Remove-Item test/tce-checksums.txt
 git add tools/chocolateyinstall.ps1
 git add tanzu-community-edition.nuspec
  
-git commit -s -m 'auto-generated - update tce choco install scripts for version ${version}'
+git commit -s -m "auto-generated - update tce choco install scripts for version ${version}"
  
 git push origin $PR_BRANCH
  
-gh pr create --repo ${TCE_REPO} --title 'auto-generated - update tce choco install scripts for version ' + $version --body 'auto-generated - update tce choco install scripts for version ' + ${version}
+gh pr create --repo ${TCE_REPO} --title "auto-generated - update tce choco install scripts for version $version" --body "auto-generated - update tce choco install scripts for version ${version}"
  
 gh pr merge --repo $TCE_REPO $PR_BRANCH --squash --delete-branch --auto
  
