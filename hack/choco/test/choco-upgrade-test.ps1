6a344ca9e752ce45bee197c3252aac31828aae1e
Aman Sharma <amansh@vmware.com>
2022-03-02 12:34:23 +0530

    testing
    Signed-off-by: Aman Sharma <amansh@vmware.com>

--- hack/choco/test/choco-upgrade-test.ps1
+++ hack/choco/test/choco-upgrade-test.ps1
@@ -85,7 +85,7 @@ git config -global credential.helper unset
 
 git push origin $PR_BRANCH
  
-gh pr create --repo ${TCE_REPO} --head "Choco: Update" --title "auto-generated - update tce choco install scripts for version $version" --body "auto-generated - update tce choco install scripts for version ${version}"
+gh pr create --repo ${TCE_REPO} --title "auto-generated - update tce choco install scripts for version $version" --body "auto-generated - update tce choco install scripts for version ${version}"
  
 gh pr merge --repo $TCE_REPO $PR_BRANCH --squash --delete-branch --auto
  
