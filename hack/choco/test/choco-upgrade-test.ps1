fb00be7d8d36420e0e0af1b67db4d4a08d189db9
Aman Sharma <56108587+aman556@users.noreply.github.com>
2022-03-16 19:28:25 +0530

    Update choco-upgrade-test.ps1

--- hack/choco/test/choco-upgrade-test.ps1
+++ hack/choco/test/choco-upgrade-test.ps1
@@ -84,7 +84,19 @@ Set-Content -Path .\tools\chocolateyinstall.ps1 -Value $textchocoinstall
 & test\e2e-test.ps1
 
 Remove-Item test/tce-checksums.txt
-git config --global --unset credential.helper
+
+# By default the ssh-agent service is disabled. Allow it to be manually started for the next step to work.
+# Make sure you're running as an Administrator.
+Get-Service ssh-agent | Set-Service -StartupType Manual
+
+# Start the service
+Start-Service ssh-agent
+
+# This should return a status of Running
+Get-Service ssh-agent
+
+# Now load your key files into ssh-agent
+ssh-add ~\.ssh\ssh-private-key
 
 git add tools/chocolateyinstall.ps1
 git add tanzu-community-edition.nuspec
