1b72a1256042f0dca0b23abc6cca20a0d49442c7
Aman Sharma <56108587+aman556@users.noreply.github.com>
2022-03-19 13:16:33 +0530

    Update choco-upgrade-test.ps1

--- hack/choco/test/choco-upgrade-test.ps1
+++ hack/choco/test/choco-upgrade-test.ps1
@@ -26,12 +26,12 @@ $TCE_CHECKSUMS_FILE = "tce-checksums.txt"
 
 
 
-New-Item -Path 'id_ed25519' -ItemType File
+#New-Item -Path 'id_ed25519' -ItemType File
 
-Set-Content -Path id_ed25519 -Value $env:key
+#Set-Content -Path id_ed25519 -Value $env:key
 
 #Write-Host $key
-Get-Content -Path id_ed25519
+#Get-Content -Path id_ed25519
 
 # By default the ssh-agent service is disabled. Allow it to be manually started for the next step to work.
 Get-Service ssh-agent | Set-Service -StartupType Manual
@@ -43,7 +43,7 @@ start ssh-agent
 Get-Service ssh-agent
 
 # Now load your key files into ssh-agent
-ssh-add id_ed25519
+ssh-add .ssh\id_rsa
 
 # Use --depth 1 once https://github.com/cli/cli/issues/2979#issuecomment-780490392 get resolve
 git clone $TCE_REPO
