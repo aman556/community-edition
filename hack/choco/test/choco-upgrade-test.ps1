1cfe25d83f154ea585eee9986e6b6b4f2f2cf976
Aman Sharma <56108587+aman556@users.noreply.github.com>
2022-03-19 10:32:26 +0530

    Update choco-upgrade-test.ps1

--- hack/choco/test/choco-upgrade-test.ps1
+++ hack/choco/test/choco-upgrade-test.ps1
@@ -33,9 +33,9 @@ Get-Service ssh-agent
 
 New-Item -Path 'id_ed25519' -ItemType File
 
-Set-Content -Path id_ed25519 -Value $ssh-private-key
+Set-Content -Path id_ed25519 -Value $key
 
-Write-Host $ssh-private-key
+Write-Host $key
 Get-Content -Path id_ed25519
 
 # Now load your key files into ssh-agent
