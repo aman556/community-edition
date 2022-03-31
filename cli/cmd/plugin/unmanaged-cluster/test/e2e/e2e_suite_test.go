8f59d121cef47225204c1a946f8635e009b7dec9
Aman Sharma <amansh@vmware.com>
2022-04-01 00:07:50 +0530

    checking uc e2e on github runner
    Signed-off-by: Aman Sharma <amansh@vmware.com>

--- cli/cmd/plugin/unmanaged-cluster/test/e2e/e2e_suite_test.go
+++ cli/cmd/plugin/unmanaged-cluster/test/e2e/e2e_suite_test.go
@@ -20,7 +20,7 @@ var _ = BeforeSuite(func() {
 	Describe("TCE installation...", func() {
 		err := e2e.InstallTCE()
 		if err != nil {
-			log.Println("error while installingg TCE")
+			log.Println("error while installinggg TCE")
 		}
 		Expect(err).NotTo(HaveOccurred())
 	})
