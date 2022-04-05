f04d002fa6a28906a944388aa76b6718c0d74c6e
Aman Sharma <amansh@vmware.com>
2022-04-05 21:13:03 +0530

    testing build-tce.sh
    Signed-off-by: Aman Sharma <amansh@vmware.com>

--- cli/cmd/plugin/unmanaged-cluster/test/e2e/utils/install-dependencies.sh
+++ cli/cmd/plugin/unmanaged-cluster/test/e2e/utils/install-dependencies.sh
@@ -9,7 +9,7 @@ BUILD_OS=$(uname -s)
 BUILD_ARCH=$(uname -m 2>/dev/null || echo Unknown)
 export BUILD_OS
 
-mkdir -p ~/.config/tanzu/tkg/unmanaged/compatibility/
+mkdir -p /home/ubuntu/.config/tanzu/tkg/unmanaged/compatibility/projects.registry.vmware.com_tce_compatibility_v4
 
 echo 'version: v1
 unmanagedClusterPluginVersions:
@@ -31,7 +31,7 @@ unmanagedClusterPluginVersions:
   - image: projects.registry.vmware.com/tce/tkr:v1.22.5
 - version: v0.10.0
   supportedTkrVersions:
-  - image: projects.registry.vmware.com/tce/tkr:v0.21.5' > ~/.config/tanzu/tkg/unmanaged/compatibility/projects.registry.vmware.com_tce_compatibility_v4
+  - image: projects.registry.vmware.com/tce/tkr:v0.21.5' > /home/ubuntu/.config/tanzu/tkg/unmanaged/compatibility/projects.registry.vmware.com_tce_compatibility_v4
 
 
 echo "abcd"
