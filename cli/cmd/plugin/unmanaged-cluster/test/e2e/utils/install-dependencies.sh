f65b4c4a5755bd29e0efd90ce42dfc0336a32707
Aman Sharma <amansh@vmware.com>
2022-04-05 10:59:30 +0530

    testing build-tce.sh
    Signed-off-by: Aman Sharma <amansh@vmware.com>

--- cli/cmd/plugin/unmanaged-cluster/test/e2e/utils/install-dependencies.sh
+++ cli/cmd/plugin/unmanaged-cluster/test/e2e/utils/install-dependencies.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# Copyright 2022 VMware Tanzu Community Edition contributors. All Rights Reserved.
+# Copyright 2021 VMware Tanzu Community Edition contributors. All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0
 
-# This script installs dependencies needed for running build-tce.sh
+# This script installs dependencies needed for running build-tce.sh and deploy-tce.sh
 
 BUILD_OS=$(uname -s)
 BUILD_ARCH=$(uname -m 2>/dev/null || echo Unknown)
@@ -11,9 +11,9 @@ export BUILD_OS
 
 # Make sure docker is installed
 echo "Checking for Docker..."
-if [ -z "$(command -v docker)" ]; then
+if [[ -z "$(command -v docker)" ]]; then
     echo "Installing Docker..."
-    if [ "${BUILD_OS}" == "Linux" ]; then
+    if [[ "$BUILD_OS" == "Linux" ]]; then
         sudo apt-get update > /dev/null
         sudo apt-get install -y \
             apt-transport-https \
@@ -31,23 +31,19 @@ if [ -z "$(command -v docker)" ]; then
         sudo service docker start
         sleep 30s
 
-        if [ "$(id -u)" -ne 0 ]; then
+        if [[ $(id -u) -ne 0 ]]; then
             sudo usermod -aG docker "$(whoami)"
         fi
-    elif [ "${BUILD_OS}" == "Darwin" ]; then
+    elif [[ "$(BUILD_OS)" == "Darwin" ]]; then
         /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
-        brew install docker
-        brew install docker-machine
-        brew install virtualbox --cask
-        docker-machine create --driver virtualbox default
-        eval "$(docker-machine env default)"
+        brew cask install docker
     fi
 else
     echo "Found Docker!"
 fi
 
 echo "Verifying Docker..."
-if ! docker run --rm hello-world > /dev/null; then
+if ! sudo docker run --rm hello-world > /dev/null; then
     error "Unable to verify docker functionality, make sure docker is installed correctly"
     exit 1
 else
@@ -56,20 +52,20 @@ fi
 
 # Make sure kubectl is installed
 echo "Checking for kubectl..."
-if [ -z "$(command -v kubectl)" ]; then
+if [[ -z "$(command -v kubectl)" ]]; then
     echo "Installing kubectl..."
-    if [ "${BUILD_OS}" == "Linux" ]; then
+    if [[ "$BUILD_OS" == "Linux" ]]; then
         curl -LO https://dl.k8s.io/release/v1.20.1/bin/linux/amd64/kubectl
-    elif [ "${BUILD_OS}" == "Darwin" ]; then
-        if [ "${BUILD_ARCH}" == "x86_64" ]; then
+    elif [[ "$BUILD_OS" == "Darwin" ]]; then
+        if [[ "$BUILD_ARCH" == "x86_64" ]]; then
             curl -LO https://dl.k8s.io/release/v1.20.1/bin/darwin/amd64/kubectl
-        elif [ "${BUILD_ARCH}" == "arm64" ]; then
+        elif [[ "$BUILD_ARCH" == "arm64" ]]; then
             curl -LO https://dl.k8s.io/release/v1.20.1/bin/darwin/arm64/kubectl
         else
-            error "${BUILD_OS}-${BUILD_ARCH} NOT SUPPORTED!!!"
+            error "$BUILD_OS-$BUILD_ARCH NOT SUPPORTED!!!"
         fi
     else
-        error "${BUILD_OS} NOT SUPPORTED!!!"
+        error "$BUILD_OS NOT SUPPORTED!!!"
         exit 1
     fi
     sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
